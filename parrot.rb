c30ddb74304e0654a8583d5bf87f1db3326ff862
Joseph Maximo Marguello <marguellomax@gmail.com>
2020-03-09 19:52:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+ phrase = parrot("Pretty bird!")
+
+def parrot 
+ 
+ puts "Squawk!"
+  return "Squawk!"
+ 
+   puts "Pretty bird!"
+    return "Pretty bird!"
+end
